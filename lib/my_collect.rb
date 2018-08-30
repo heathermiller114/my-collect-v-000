60d62e1ebba3af31f0cc026d49710ebf4a8c4053
Heather Miller <heather.miller114@gmail.com>
2018-08-30 00:53:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,10 @@ def my_collect(list)
   counter = 0
   caps_lang = []
   while list.length > 0
-    caps_lang << yield(array[counter])
+    caps_lang << yield(list[counter])
     counter += 1
   end
   caps_lang
 end
 
-my_collect(array) { |language| language.upcase }
+my_collect(list) { |language| language.upcase }
